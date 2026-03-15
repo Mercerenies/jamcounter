c7ed4f3a3cfbb3190deaa01914d442748c1b5354
Mercerenies <mercerenies@gmail.com>
2026-03-14 22:20:03 -0400

    adjust clustering algorithm (some people don't get spaces right in author names)

--- src/clustering/games.rs
+++ src/clustering/games.rs
@@ -7,7 +7,5 @@ pub const COMPARE_THRESHOLD: f64 = 0.6;
 pub fn game_comparison_score(a: &VideoGame, b: &VideoGame) -> f64 {
   let title_score = token_containment_score(&a.title, &b.title);
   let author_score = token_containment_score(&a.author, &b.author);
-  // Weigh the author a little heavier (I think the author is more
-  // likely to match)
-  title_score * 0.35 + author_score * 0.65
+  title_score * 0.7 + author_score * 0.3
 }
